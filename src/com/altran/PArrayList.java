cf8a6f49eab5669986c6b5d3bbdaac915e1ccb63
Prinkal <sahupinku05@gmail.com>
2020-03-03 17:35:38 +0530

    Update PArrayList.java

--- src/com/altran/PArrayList.java
+++ src/com/altran/PArrayList.java
@@ -1,7 +1,9 @@
 package com.altran;
 
-public class PArrayList<T> {
+import java.util.ArrayList;
+import java.util.Arrays;
 
+public class PArrayList<E> {
     private int HEAD = -1;
     private Object arr[];
 
@@ -9,17 +11,51 @@ public class PArrayList<T> {
         arr = new Object[10];
     }
 
-    public void add(T e) {
+    public void add(E e) {
+        checkAndIncreaseSize();
         HEAD++;
         arr[HEAD] = e;
     }
 
-    public void remove(int pos) throws ArrayIndexOutOfBoundsException {
-        if (pos >= arr.length) {
-            throw new ArrayIndexOutOfBoundsException(pos);
+    private void checkAndIncreaseSize() {
+        if (HEAD == arr.length - 1) {
+            //right shift operator which means half of the value 6>>1 means 3
+            int newLength = arr.length + (arr.length >> 1);
+            arr = Arrays.copyOf(arr, newLength);
+            System.out.println(Arrays.toString(arr));
         }
-
-
     }
 
+    public void remove(int pos) throws ArrayIndexOutOfBoundsException {
+        checkInavlidPosition(pos);
+
+        if (HEAD != 0 || HEAD != pos) {
+            System.arraycopy(arr, pos + 1, arr, pos, (arr.length - pos - 1));
+        }
+        arr[HEAD] = null;
+        HEAD--;
+    }
+
+    private void checkInavlidPosition(int pos) {
+        if (pos > HEAD || pos < 0) {
+            throw new ArrayIndexOutOfBoundsException(pos);
+        }
+    }
+
+    public E get(int pos){
+        checkInavlidPosition(pos);
+        return (E) arr[pos];
+    }
+
+    public int getSize() {
+        return HEAD + 1;
+    }
+
+    @Override
+    public String toString() {
+        return "PArrayList{" +
+                "HEAD=" + HEAD +
+                ", arr=" + Arrays.toString(arr) +
+                '}';
+    }
 }
