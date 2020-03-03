89198e7c153e50329803fca4843bf5e46a0bfaee
Prinkal <sahupinku05@gmail.com>
2020-03-03 17:36:00 +0530

    Update Main.java

--- src/com/altran/Main.java
+++ src/com/altran/Main.java
@@ -1,8 +1,26 @@
 package com.altran;
 
+import java.util.ArrayList;
+
 public class Main {
 
     public static void main(String[] args) {
         // write your code here
+        PArrayList<Integer> pArrayList = new PArrayList<Integer>();
+        for (int i = 0; i < 12; i++) {
+            pArrayList.add(i);
+        }
+        System.out.println(pArrayList.toString());
+
+        // pArrayList.remove(5);
+        // System.out.println(pArrayList.toString());
+
+        pArrayList.remove(-2);
+
+        for (int i = 0; i < 12; i++) {
+
+            pArrayList.remove(pArrayList.getSize() - 1);
+            System.out.println(pArrayList.toString());
+        }
     }
 }
