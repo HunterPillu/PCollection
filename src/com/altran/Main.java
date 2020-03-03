cb8d0f4a96925ae4f27fac5333a2b9875058f2d3
WarFly <sahupinku05@gmail.com>
2020-03-03 10:22:20 +0530

    update

--- src/com/altran/Main.java
+++ src/com/altran/Main.java
@@ -1,8 +1,25 @@
 package com.altran;
 
+import com.google.gson.Gson;
+
 public class Main {
 
     public static void main(String[] args) {
         // write your code here
+
+        PArrayList<Integer> pal = new PArrayList<>();
+        pal.add(1);
+        pal.add(2);
+        pal.add(3);
+        pal.add(4);
+        pal.add(5);
+        pal.add(6);
+        pal.add(7);
+        pal.add(8);
+        pal.add(9);
+        pal.add(10);
+        System.out.println(pal.toString());
+        pal.remove(9);
+        System.out.println(pal.toString());
     }
 }
--- src/com/altran/PArrayList.java
+++ src/com/altran/PArrayList.java
@@ -1,5 +1,7 @@
 package com.altran;
 
+import java.util.Arrays;
+
 public class PArrayList<T> {
 
     private int HEAD = -1;
@@ -10,16 +12,53 @@ public class PArrayList<T> {
     }
 
     public void add(T e) {
+        checkAndIncreaseSize();
         HEAD++;
         arr[HEAD] = e;
     }
 
-    public void remove(int pos) throws ArrayIndexOutOfBoundsException {
-        if (pos >= arr.length) {
-            throw new ArrayIndexOutOfBoundsException(pos);
+    private void checkAndIncreaseSize() {
+        if (HEAD == arr.length - 1) {
+            Object[] arr2 = new Object[getNewSize()];
+            for (int i = 0; i < arr.length; i++) {
+                arr2[i] = arr[i];
+            }
+            arr = arr2;
         }
-
-
     }
 
+    private int getNewSize() {
+        return ((3 * arr.length) / 2) + 1;
+    }
+
+    private void checckInvalidPosition(int pos) throws ArrayIndexOutOfBoundsException {
+        if (0 > pos && HEAD != -1 && pos > HEAD) {
+            throw new ArrayIndexOutOfBoundsException(pos);
+        }
+    }
+
+    public void remove(int pos) {
+        checckInvalidPosition(pos);
+        arr[pos] = null;
+        if (pos != HEAD) {
+            swapData(pos);
+        }
+        HEAD--;
+    }
+
+    public void swapData(int pos) {
+
+        for (int i = pos; i < HEAD; i++) {
+            arr[i] = arr[i + 1];
+        }
+        arr[HEAD] = null;
+    }
+
+    @Override
+    public String toString() {
+        return "PArrayList{" +
+                "HEAD=" + HEAD +
+                ", arr=" + Arrays.toString(arr) +
+                '}';
+    }
 }
