35f35bf9d9ac66e75a48ba02c11a7bba1b929ac5
Prinkal <sahupinku05@gmail.com>
2020-03-06 16:55:54 +0530

    Update Main.java

--- src/com/altran/Main.java
+++ src/com/altran/Main.java
@@ -20,23 +20,27 @@ public class Main {
         for (int i = 0; i < 12; i++) {
 
             pArrayList.remove(pArrayList.size() - 1);
-            System.out.println(pArrayList.toString());
-        }
 
+        }
+        System.out.println(pArrayList.toString());
         PArrayList<User> al = new PArrayList<>(10);
         al.add(new User("asdf", 45));
         al.add(new User("asdf", 47));
-        System.out.println(al.toString());
+        //System.out.println(al.toString());
 
-        // PArrayList<User> al2 = new PArrayList<>(al);
+
 
         al.add(0, new User("asdfae", 46));
-        System.out.println(al.toString());
+        //System.out.println(al.toString());
 
         al.add(2, new User("asdfae", 48));
-        System.out.println(al.toString());
+        //System.out.println(al.toString());
 
         al.add(3, new User("asdfae", 48));
-        System.out.println(al.toString());
+        //System.out.println(al.toString());
+
+        PArrayList<User> al2 = new PArrayList<>(al);
+        //al2.addAll(al);
+        System.out.println(al2.toString());
     }
 }
